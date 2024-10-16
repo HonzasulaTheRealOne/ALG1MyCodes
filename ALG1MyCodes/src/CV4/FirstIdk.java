1639ef0198ae9a9a3bf790d1e2e9c1d940887136
Jan Šůla <jan.frantisek.sula@tul.cz>
2024-10-16 08:11:52 +0200

    cv5

---
+++ ALG1MyCodes/src/CV4/FirstIdk.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CV4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Pavla_2
+ */
+public class FirstIdk {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        boolean isIt = false;
+        int inputCislo = scanner.nextInt();
+        
+        for (int i = (inputCislo-1); i > 1; i--){
+            
+            if (inputCislo % i == 0){
+                System.out.println(i);
+                isIt = false;
+                break;
+            }
+            else {
+                isIt = true;
+
+        }
+        }
+        if (isIt){
+        System.out.println("Je prvočíslo");}
+        
+        else{
+                        System.out.println("Není prvočíslo");
+
+                }
+        
+    }
+}
---
+++ ALG1MyCodes/src/CV4/Trojuhelnik.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CV4;
+
+/**
+ *
+ * @author Pavla_2
+ */
+import java.util.Scanner;
+
+public class Trojuhelnik {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Zadejte počet hvězdiček: ");
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+                System.out.print("* ");
+            }
+        
+        
+        
+        /*
+         33
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Zadejte délku strany čtverce: ");
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+        */
+        
+        
+        
+        
+        
+        
+        
+        /*
+        
+        34
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Zadejte výšku trojúhelníku: ");
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+*/
+        
+        /*
+        
+        
+        35
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Zadejte výšku trojúhelníku: ");
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+*/
+        }
+    }
+
+        
+        
+        
+    
+
